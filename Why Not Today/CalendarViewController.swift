7f7004c5d57d57d46b76e722087c0689b1ca35c3
Taylor Ray Howard <taylor@taylorrayhoward.com>
2017-04-12 21:32:52 -0500

    Added basic calendar with simple onclick

--- Why Not Today/CalendarViewController.swift
+++ Why Not Today/CalendarViewController.swift
@@ -1,43 +1,61 @@
 //
 //  ViewController.swift
-//  TestJTApple
+//  Why Not Today
 //
 //  Created by Taylor Howard on 4/11/17.
-//  Copyright © 2017 Taylor Howard. All rights reserved.
+//  Copyright © 2017 TaylorRayHoward. All rights reserved.
 //
 
 import UIKit
 import JTAppleCalendar
 
 class CalendarViewController: UIViewController {
-    let formatter = DateFormatter()
     
+    @IBOutlet weak var calendarView: JTAppleCalendarView!
+    
+    var formatter = DateFormatter()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupCalendarView()
     }
     
+    func setupCalendarView() {
+        calendarView.minimumLineSpacing = 0
+        calendarView.minimumInteritemSpacing = 0
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+}
+
+extension CalendarViewController: JTAppleCalendarViewDataSource {
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        formatter.dateFormat = "yyyy MM dd"
+        formatter.locale = Calendar.current.locale
+        formatter.timeZone = Calendar.current.timeZone
+        let startDate = formatter.date(from: "2017 01 01")!
+        let endDate = formatter.date(from: "2017 12 31")!
+        let params = ConfigurationParameters(startDate: startDate, endDate: endDate)
+        return params
+    }
     
 }
 
-extension CalendarViewController: JTAppleCalendarViewDataSource, JTAppleCalendarViewDelegate {
-    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
-        formatter.dateFormat = "yyyy MM dd"
-        let startDate = formatter.date(from: "2017 01 01")!
-        let endDate = formatter.date(from: "2017 12 31")!
-        
-        let config = ConfigurationParameters(startDate: startDate, endDate: endDate)
-        return config
-    }
-    
-    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+extension CalendarViewController: JTAppleCalendarViewDelegate {
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState,
+                  indexPath: IndexPath) -> JTAppleCell {
         let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CustomCell", for: indexPath) as! CustomCell
         cell.dateLabel.text = cellState.text
         return cell
     }
+    
+    func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+        guard let validCell = cell as? CustomCell else { return }
+        validCell.circleView.isHidden = !validCell.circleView.isHidden
+    }
+    
 }
