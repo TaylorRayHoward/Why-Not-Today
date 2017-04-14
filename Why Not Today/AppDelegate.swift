21d304fcc1b081b83906785f776d427ea9be4c37
Taylor Ray Howard <taylor@taylorrayhoward.com>
2017-04-13 23:43:31 -0500

    Added calendar, scrolls to current date
    + The calendar now defaults to current dates
    + In and out dates are colored differently
    
    - Took away selections on the calendar for now

--- Why Not Today/AppDelegate.swift
+++ Why Not Today/AppDelegate.swift
@@ -41,7 +41,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // Now that we've told Realm how to handle the schema change, opening the file
         // will automatically perform the migration
-        let realm = try! Realm()
+        _ = try! Realm()
     return true
     }
 
--- Why Not Today/CalendarViewController.swift
+++ Why Not Today/CalendarViewController.swift
@@ -11,6 +11,8 @@ import JTAppleCalendar
 
 class CalendarViewController: UIViewController {
     
+    @IBOutlet weak var monthLabel: UILabel!
+    @IBOutlet weak var yearLabel: UILabel!
     @IBOutlet weak var calendarView: JTAppleCalendarView!
     
     var formatter = DateFormatter()
@@ -19,6 +21,11 @@ class CalendarViewController: UIViewController {
         super.viewDidLoad()
         
         setupCalendarView()
+        calendarView.visibleDates { (visibleDates) in
+            self.setMonthLabel(from: visibleDates)
+            self.setYearLabel(from: visibleDates)
+        }
+        calendarView.scrollToDate(Date())
     }
     
     func setupCalendarView() {
@@ -30,6 +37,38 @@ class CalendarViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func setMonthLabel(from visibleDates: DateSegmentInfo) {
+        let date = visibleDates.monthDates.first!.date
+        formatter.dateFormat = "MMMM"
+        monthLabel.text = formatter.string(from: date)
+    }
+    
+    func setYearLabel(from visibleDates: DateSegmentInfo) {
+        let date = visibleDates.monthDates.first!.date
+        yearLabel.text =  "\(date.yearFromDate()!)"
+    }
+    
+    func changeCellDisplay(_ cell: CustomCell, with calendar: JTAppleCalendarView, withState cellState: CellState) {
+        cell.dateLabel.text = cellState.text
+        
+//        TODO cellState.day in our list of dates
+        
+//        if cellState.date in list of dates {
+//            cell.circleView.isHidden = false
+//        }
+//        else {
+//            cell.circleView.isHidden = true
+//        }
+        
+        if cellState.dateBelongsTo != .thisMonth {
+            cell.dateLabel.textColor = UIColor.gray
+        }
+        else {
+            cell.dateLabel.textColor = UIColor(rgb: 0xD9E5D6)
+        }
+    }
 }
 
 extension CalendarViewController: JTAppleCalendarViewDataSource {
@@ -37,25 +76,37 @@ extension CalendarViewController: JTAppleCalendarViewDataSource {
         formatter.dateFormat = "yyyy MM dd"
         formatter.locale = Calendar.current.locale
         formatter.timeZone = Calendar.current.timeZone
-        let startDate = formatter.date(from: "2017 01 01")!
-        let endDate = formatter.date(from: "2017 12 31")!
-        let params = ConfigurationParameters(startDate: startDate, endDate: endDate)
+        
+        
+        var components = DateComponents()
+        components.year = 2016
+        components.month = 1
+        components.day = 1
+        let startDate = Calendar.current.date(from: components)!
+        let params = ConfigurationParameters(startDate: startDate, endDate: Date())
+        
         return params
     }
-    
 }
 
 extension CalendarViewController: JTAppleCalendarViewDelegate {
     func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState,
                   indexPath: IndexPath) -> JTAppleCell {
         let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CustomCell", for: indexPath) as! CustomCell
-        cell.dateLabel.text = cellState.text
+        changeCellDisplay(cell, with: calendarView, withState: cellState)
         return cell
+    }
+    func calendar(_ calendar: JTAppleCalendarView, didScrollToDateSegmentWith visibleDates: DateSegmentInfo) {
+        setMonthLabel(from: visibleDates)
+        setYearLabel(from: visibleDates)
     }
     
     func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
-        guard let validCell = cell as? CustomCell else { return }
-        validCell.circleView.isHidden = !validCell.circleView.isHidden
+//        guard let validCell = cell as? CustomCell else { return }
+//        TODO Show list of Habits that they have created and then allow them to say completed or not completed
     }
     
+    func calendar(_ calendar: JTAppleCalendarView, didDeselectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+//        guard let validCell = cell as? CustomCell else { return }
+    }
 }
