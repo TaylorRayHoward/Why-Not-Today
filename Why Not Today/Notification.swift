870fc39719c2551829d9171506a4ca6645debbdf
Taylor Howard <taylorhoward10@gmail.com>
2017-04-30 22:07:11 -0500

    If in past, schedule for for tomorrow

--- Why Not Today/Notification.swift
+++ Why Not Today/Notification.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import RealmSwift
+import UserNotifications
 
 class Notification: Object {
     dynamic var FireTime = Date()
@@ -23,5 +24,33 @@ class Notification: Object {
         self.FireTime = date
         self.Message = label
         self.id = UUID().uuidString
+    }
+}
+
+func cancelNotifs() {
+    UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+}
+
+func setupNotificaiton(for notif: Notification) {
+    let content = UNMutableNotificationContent()
+    content.title = "Why Not Today Reminder"
+    content.subtitle = "Daily Reminder"
+    content.body = notif.Message
+    
+    var date = DateComponents()
+    date.hour = notif.FireTime.getHour()!
+    date.minute = notif.FireTime.getMinute()!
+    let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: false)
+    let request = UNNotificationRequest(identifier: notif.id, content: content, trigger: trigger)
+    UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+}
+
+func postponeNotifications(for date: Date) {
+    let notifications = DBHelper.sharedInstance.getAll(ofType: Notification.self)
+    let tomorrow = Calendar.current.date(byAdding: .day, value: 1, to: date)
+    cancelNotifs()
+    for object in notifications {
+        let notif = object as! Notification
+        setupNotificaiton(for: notif)
     }
 }
